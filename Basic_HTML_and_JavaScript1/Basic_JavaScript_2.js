cf48defb88c8609b23929272048780ec20808800
DamonFlick <75501983+DamonFlick@users.noreply.github.com>
2020-12-08 16:51:40 -0800

    Create Basick HTML and JS assignments

---
+++ Basic_HTML_and_JavaScript1/Basic_JavaScript_2.js
@@ -0,0 +1,6 @@
+function myFirstFunction() {
+				var String = "Kiss me, I'm Irish!";
+				var result = String.fontcoloe("green");
+				document.getElementById("Irish").innerHTML =
+				result;
+			}
